b8859481d7a98117e32101dbef7b7b5856b83c1b
CharlesAgra <csagra216@gmail.com>
2018-01-23 00:02:24 -0200

    Something changes

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -3,9 +3,19 @@ var buttonOne = document.querySelector('#start');
 var buttonTwo = document.querySelector('#stop');
 var buttonThree = document.querySelector('#reset');
 
-var seconds = 60;
-var minutes = 24;
+var seconds;
+var minutes;
 var interval;
+var pomodoroFoiClicado;
+var shortBreakFoiClicado;
+var longBreakFoiClicado;
+
+var audio = document.createElement('audio');
+audio.src = './hey-brother.mp3';
+
+var pomodoroId = document.querySelector('#pomodoro');
+var shortBreakId = document.querySelector('#short-break');
+var longBreakId = document.querySelector('#long-break');
 
 function startTimer(){
 	interval = setInterval(timerIt, 1000);
@@ -13,14 +23,16 @@ function startTimer(){
 
 function timerIt(){
 	seconds--;
-	/*tag.innerHTML = minutes+':'+seconds;*/
 	
 	var a = new String(seconds);
 	var b = new String(minutes);
-
 	verify(a, b);
 
-	if(seconds == 0){
+
+	if(seconds == 0 && minutes == 0){
+		audio.play();
+		stopTimer();
+	}else if(seconds == 0){
 		minutes--;
 		seconds = 60;
 	}
@@ -40,21 +52,57 @@ function verify(segundos, minutos){
 	}
 }
 
-
 function stopTimer(){
 	clearInterval(interval);
 }
 
 function resetTimer(){
-	minutes = 24;
-	seconds = 60;
-	tag.innerHTML = '25:00';
+	if(pomodoroFoiClicado == true){
+		tag.innerHTML = '25:00';
+		seconds = 60;
+		minutes = 24;
+	}else if(shortBreakFoiClicado == true){
+		tag.innerHTML = '5:00';
+		seconds = 60;
+		minutes = 4;
+	}else if(longBreakFoiClicado == true){
+		tag.innerHTML = '10:00';
+		seconds = 60;
+		minutes = 9;
+	}else{
+		//go on
+	}
 }
 
-function changeLink(){
+function pomodoro(){
+	tag.innerHTML = '25:00';
+	seconds = 60;
+	minutes = 24;
+	pomodoroFoiClicado = true;
+	shortBreakFoiClicado = false;
+	longBreakFoiClicado = false;
+
+	startTimer();
+}
+
+function shortBreak(){
 	tag.innerHTML = '5:00';
 	seconds = 60;
 	minutes = 4;
+	pomodoroFoiClicado = false;
+	shortBreakFoiClicado = true;
+	longBreakFoiClicado = false;
+
+	startTimer();
+}
+
+function longBreak(){
+	tag.innerHTML = '10:00';
+	seconds = 60;
+	minutes = 9;
+	pomodoroFoiClicado = false;
+	shortBreakFoiClicado = false;
+	longBreakFoiClicado = true;
 
 	startTimer();
 }
